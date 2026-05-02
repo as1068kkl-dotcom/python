b202154e14a7f2c1d36f3008fa05e6d3c68adff4
as1068kkl-dotcom <as1068.kkl@ked.edu.in>
2026-05-02 16:04:11 +0530

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,8 @@
+#print a sentence
+print('Welcome to the world of programming!')
+#print a number
+print(4)
+#Next line after print
+print("hello phyton\n")
+print("hello",5)
+print ("welcome to ", end="*")
